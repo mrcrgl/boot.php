e9119c9cc626dfd14112b15ecdfd5ccefb1e9e7c
Marc Riegel <mail@marclab.de>
2012-09-21 11:21:26 +0000

    bugfixes in validator

--- library/versions/utilities/validator.inc.php
+++ library/versions/utilities/validator.inc.php
@@ -12,39 +12,39 @@
  * --
  */
 class Validator {
-  
+
   public static $error;
   public static $errors;
-  
+
   public static function setError($error) {
     Validator::$error    = $error;
     Validator::$errors[] = $error;
   }
-  
+
   public static function resetErrors() {
     Validator::$error    = '';
     Validator::$errors[] = array();
   }
-  
+
   public static function is($value, $processor, $minlen=false, $maxlen=false) {
     if ($minlen && strlen($value) < $minlen) {
       Validator::setError("MinLengthExceeded");
       return false;
     }
-    
+
     if ($maxlen && strlen($value) > $maxlen) {
       Validator::setError("MaxLengthExceeded");
       return false;
     }
-    
+
     $processor = 'is_'.$processor;
     if (!Validator::$processor($value)) {
       return false;
     }
-    
+
     return true;
   }
-  
+
   public static function checkField($field_declaration, $value) {
   	if ($field_declaration->get('null') === true && Validator::is($value, 'null')) {
   		return true;
@@ -54,26 +54,26 @@ class Validator {
   	}
   	if (in_array($field_declaration->get('type'), array('integer', 'string', 'float', 'decimal'))) {
 	  	if (!Validator::is($value, $field_declaration->get('type'), $field_declaration->get('min_length'), $field_declaration->get('max_length'))) {
-	  		printf("%s with length %d is not accepted; minlength %d and maxlength %d expected".NL, $field_declaration->get('type'), strlen($value), $field_declaration->get('min_length'), $field_declaration->get('max_length'));
+	  		#printf("%s with length %d is not accepted; minlength %d and maxlength %d expected".NL, $field_declaration->get('type'), strlen($value), $field_declaration->get('min_length'), $field_declaration->get('max_length'));
   			return false;
 	  	}
   	}
-  	
+
   	foreach ($field_declaration->get('validators') as $validator) {
   		if (!Validator::is($value, $validator)) {
-  			printf("$validator is required".NL);
+  			#printf("$validator is required".NL);
   			return false;
   		}
   	}
-  	
+
   	return true;
-  } 
-  
+  }
+
   public static function regexp($value, $regexp, $isTrueIfEmpty=false, $isCaseSensitive=false) {
     if (!Validator::is($value, 'filled') && $isTrueIfEmpty) {
       true;
     }
-    
+
     if ($isCaseSensitive) {
       if (!ereg($regexp, $value)) {
         Validator::setError("ExpressionNotMatched");
@@ -85,71 +85,71 @@ class Validator {
         return false;
       }
     }
-    
+
     return false;
   }
-  
-  
+
+
   /***************************************************
    *
    * Validator Module
    *
    ***************************************************/
-  
+
   public static function is_integer($value) {
     if (is_numeric($value)) {
       return true;
     }
-    
+
     Validator::setError("IntegerExpected");
     return false;
   }
-  
+
 	public static function is_string($value) {
     if (is_string($value)) {
       return true;
     }
-    
+
     Validator::setError("IntegerExpected");
     return false;
   }
-  
+
   public static function is_filled($value) {
     if (strlen($value) > 0) {
       return true;
     }
-    
+
     Validator::setError("StringIsEmpty");
     return false;
   }
-  
+
 	public static function is_blank($value) {
     if (strlen($value) == 0) {
       return true;
     }
-    
+
     Validator::setError("StringIsEmpty");
     return false;
   }
-  
+
 	public static function is_array($value) {
     if (is_array($value)) {
       return true;
     }
-    
+
     Validator::setError("ArrayExcepted");
     return false;
   }
-  
+
 	public static function is_null($value) {
     if (is_null($value)) {
       return true;
     }
-    
+
     Validator::setError("StringIsEmpty");
     return false;
   }
-  
+
   public static function is_uid($value) {
     if (self::is_intuid($value)) {
       return true;
@@ -157,59 +157,59 @@ class Validator {
     if (self::is_hexuid($value)) {
       return true;
     }
-    
+
     Validator::setError("UIDExpected");
     return false;
   }
-  
+
   public static function is_intuid($value) {
     if (is_numeric($value)) {
       return true;
     }
-    
+
     Validator::setError("UIDExpected");
     return false;
   }
-  
+
   public static function is_hexuid($value) {
     if (preg_match('/^([0-9a-z]{13})$/', $value)) {
       return true;
     }
-    
+
     Validator::setError("HexUIDExpected");
     return false;
   }
-  
+
   public static function is_email($value) {
     if (filter_var($value, FILTER_VALIDATE_EMAIL)) {
       return true;
     }
-    
+
     Validator::setError("EMailExpected");
     return false;
   }
-  
+
   public static function is_ip($value) {
     if (filter_var($value, FILTER_VALIDATE_IP)) {
       return true;
     }
-    
+
     Validator::setError("IPExpected");
     return false;
   }
-  
+
   public static function is_timestamp($value) {
-    if (preg_match("/^(\d{4})-(\d{2})-(\d{2}) ([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $value, $matches)) { 
-      if (checkdate($matches[2], $matches[3], $matches[1])) { 
-        return true; 
-      } 
-    } 
-    
+    if (preg_match("/^(\d{4})-(\d{2})-(\d{2}) ([01][0-9]|2[0-3]):([0-5][0-9]):([0-5][0-9])$/", $value, $matches)) {
+      if (checkdate($matches[2], $matches[3], $matches[1])) {
+        return true;
+      }
+    }
+
     Validator::setError("MYSQL_TIMESTAMP_EXPECTED");
     return false;
   }
-  
-  
+
+
 }
 
 
