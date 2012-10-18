71e5bf660e9faa6568c83a4622416f5ca4967fcc
Marc Riegel <mail@marclab.de>
2012-10-18 23:20:15 +0200

    First steps for VComponent. Issue #1

--- library/versions/component/component.inc.php
+++ library/versions/component/component.inc.php
@@ -1,15 +1,29 @@
 <?php
 /**
+ * The VComponent class.
  *
+ * @license   MIT Licence (see LICENSE file)
+ * @copyright 2012 Marc Riegel
  *
- *
+ * @author  Marc Riegel <mail@marclab.de>
+ * @package Versions.library
+ * @subpackage Url
  */
 
 /**
+ * The VComponent class.
  *
- *
+ * @author Marc Riegel <mail@marclab.de>
  */
 class VComponent extends VObject
 {
+    /*
+        Path of component
+        Template Path if available
+        View Path if available
+        Ref: Urls
+        Ref: Controller
+        Ref: Config
+    */
     
 }
---
+++ library/versions/component/factory.inc.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * The VComponentFactory class.
+ *
+ * @license   MIT Licence (see LICENSE file)
+ * @copyright 2012 Marc Riegel
+ *
+ * @author  Marc Riegel <mail@marclab.de>
+ * @package Versions.library
+ * @subpackage Url
+ */
+
+/**
+ * The VComponentFactory class.
+ *
+ * @author Marc Riegel <mail@marclab.de>
+ */
+class VComponentFactory extends VObject
+{
+    /*
+        Fetching Components
+        Record individual Pathway
+        Fetch Component by Pathway (Step / Id)
+        Maybe, integrate core functions of VUrls
+    */
+    
+}
